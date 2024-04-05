6765a1061a99086db93b25a0032dd582676714d3
Ahsanu <caasperahsanuamala5@gmail.com>
2024-04-05 19:09:20 +0700

    [5-4-1909] adding i2c and lcdgfx back to repo, success to build

--- firmware/README.md
+++ firmware/README.md
@@ -4,5 +4,28 @@ using ESP-IDF instead arduino based platform.
 
 
 ## Note 
+Note Before First Build, remove **required** from lcdgfx `CMakeLists.txt`
+
+```cmake
+
+    idf_component_register(SRCS ${SOURCE_FILES}
+                           INCLUDE_DIRS
+                               "src"
+                           REQUIRES
+                               driver)
+```
+
+to 
+
+
+```cmake
+
+  idf_component_register(SRCS ${SOURCE_FILES}
+                           INCLUDE_DIRS
+                               "src")
+```
+
+## Reference 
+
 - [ESP8266 Toolchain Docs](https://docs.espressif.com/projects/esp8266-rtos-sdk/en/latest/get-started/index.html#setup-toolchain)
 - [Encoder.h](https://github.com/PaulStoffregen/Encoder/blob/c083e9cbd6400f7e72a794c0d371a00a09d2a25d/Encoder.h#L162)
--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -1,17 +1,3 @@
-// Copyright 2018-2025 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
@@ -20,10 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "canvas/fonts/fonts.h"
 #include "driver/uart.h"
-namespace std {
-void __attribute__((noreturn)) __throw_length_error(const char *) { abort(); }
-} // namespace std
+#include "i2c.h"
+#include "lcd_hal/esp/esp8266_i2c.h"
+#include "v2/gui/menu.h"
+#include "v2/lcd/ssd1306/lcd_ssd1306.h"
 
 static const char *TAG = "uart_events";
 
@@ -31,92 +19,33 @@ static const char *TAG = "uart_events";
 
 #define BUF_SIZE (1024)
 #define RD_BUF_SIZE (BUF_SIZE)
-static QueueHandle_t uart0_queue;
 
-static void uart_event_task(void *pvParameters) {
-  uart_event_t event;
-  uint8_t *dtmp = (uint8_t *)malloc(RD_BUF_SIZE);
+static void main_loop(void *pvParameters) {
+  /* uint8_t *dtmp = (uint8_t *)malloc(RD_BUF_SIZE); */
+
+  DisplaySSD1306_128x32_I2C display(-1);
+
+  const char *menuItems[] = {"Main", "Config", "About"};
+  LcdGfxMenu menu(menuItems, sizeof(menuItems) / sizeof(char *));
+
+  display.setFixedFont(ssd1306xled_font6x8);
+  display.begin();
 
   for (;;) {
-    // Waiting for UART event.
-    uart_write_bytes(EX_UART_NUM, (const char *)TAG, strlen(TAG));
-    if (xQueueReceive(uart0_queue, (void *)&event,
-                      (portTickType)portMAX_DELAY)) {
+    // read input rotary encoder
 
-      bzero(dtmp, RD_BUF_SIZE);
-      ESP_LOGI(TAG, "uart[%d] event:", EX_UART_NUM);
+    // read input temp
 
-      switch (event.type) {
-      // Event of UART receving data
-      // We'd better handler data event fast, there would be much more data
-      // events than other types of events. If we take too much time on data
-      // event, the queue might be full.
-      case UART_DATA:
-        ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
-        uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
-        ESP_LOGI(TAG, "[DATA EVT]:");
-        uart_write_bytes(EX_UART_NUM, (const char *)dtmp, event.size);
-        break;
-
-      // Event of HW FIFO overflow detected
-      case UART_FIFO_OVF:
-        ESP_LOGI(TAG, "hw fifo overflow");
-        // If fifo overflow happened, you should consider adding flow control
-        // for your application. The ISR has already reset the rx FIFO, As an
-        // example, we directly flush the rx buffer here in order to read more
-        // data.
-        uart_flush_input(EX_UART_NUM);
-        xQueueReset(uart0_queue);
-        break;
-
-      // Event of UART ring buffer full
-      case UART_BUFFER_FULL:
-        ESP_LOGI(TAG, "ring buffer full");
-        // If buffer full happened, you should consider encreasing your buffer
-        // size As an example, we directly flush the rx buffer here in order to
-        // read more data.
-        uart_flush_input(EX_UART_NUM);
-        xQueueReset(uart0_queue);
-        break;
-
-      case UART_PARITY_ERR:
-        ESP_LOGI(TAG, "uart parity error");
-        break;
-
-      // Event of UART frame error
-      case UART_FRAME_ERR:
-        ESP_LOGI(TAG, "uart frame error");
-        break;
-
-      // Others
-      default:
-        ESP_LOGI(TAG, "uart event type: %d", event.type);
-        break;
-      }
-    }
+    // run control scheme
   }
 
-  free(dtmp);
-  dtmp = NULL;
+  /* free(dtmp); */
+  /* dtmp = NULL; */
   vTaskDelete(NULL);
 }
 
 extern "C" void app_main();
 
 void app_main() {
-  // Configure parameters of an UART driver,
-  // communication pins and install the driver
-  uart_config_t uart_config = {.baud_rate = 74880,
-                               .data_bits = UART_DATA_8_BITS,
-                               .parity = UART_PARITY_DISABLE,
-                               .stop_bits = UART_STOP_BITS_1,
-                               .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
-  uart_param_config(EX_UART_NUM, &uart_config);
-
-  // Install UART driver, and get the queue.
-  uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 100,
-                      &uart0_queue, 0);
-
-  // Create a task to handler UART event from ISR
-  xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
+  xTaskCreate(main_loop, "uart_event_task", 2048, NULL, 12, NULL);
 }
