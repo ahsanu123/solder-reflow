147f159382deb8f203358576e2755459ee7919f5
Ahsanu <caasperahsanuamala5@gmail.com>
2024-07-14 11:23:56 +0700

    update firmware to version 2

---
+++ firmware/v1/components/lcdgfx/.travis/setup_esp_build_env.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -d ~/esp/esp-idf ]; then
+    echo "ESP IDF is already installed"
+else
+    # echo "Installing packages..."
+    # sudo apt-get install libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-click python-cryptography python-future python-pyparsing python-pyelftools cmake ninja-build ccache
+    #
+    mkdir -p ~/esp
+    # Switching to 4.3 version, since v5.0-dev doesn't support Makefiles anymore
+    git clone -b v4.3 --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+    # git clone --recursive https://github.com/espressif/esp-idf.git ~/esp/esp-idf
+fi
+
+cd ~/esp/esp-idf
+if [ "$1" = "travis" ]; then
+    ./install.sh
+fi
