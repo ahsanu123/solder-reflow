1228bef11fd29df8ccfea51e5efda264d503f034
Ahsanu <caasperahsanuamala5@gmail.com>
2025-06-11 21:02:28 +0700

    [06-11-2101] scafold esp project with embassy and defmt, use esp-generate
    esp-generate --chip esp32 -o alloc -o embassy -o defmt -o unstable-hal
    Sore-v3

---
+++ firmware/v3/README.md
@@ -0,0 +1,5 @@
+
+
+## Note 
+
+- `esp-generate --chip esp32 -o alloc -o embassy -o defmt -o unstable-hal Sore-v3` 
---
+++ firmware/v3/src/bin/main.rs
@@ -0,0 +1,63 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::timg::TimerGroup;
+use esp_wifi::ble::controller::BleConnector;
+use bt_hci::controller::ExternalController;
+
+ use esp_println as _;
+ use defmt::info;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    // generator version: 0.4.0
+
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let timer0 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    info!("Embassy initialized!");
+
+    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let wifi_init = esp_wifi::init(timer1.timer0, rng, peripherals.RADIO_CLK)
+        .expect("Failed to initialize WIFI/BLE controller");
+    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
+    let transport = BleConnector::new(&wifi_init, peripherals.BT);
+    let _ble_controller = ExternalController::<_, 20>::new(transport);
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.1/examples/src/bin
+}
